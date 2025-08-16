d7400e70abe8c67a9ccb1a8f86d4890d5d5bc7c2
SHIHAB-HUB <si.shihab2005@gmail.com>
2025-08-16 15:08:59 +0600

    Bug Fix

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ print("###############################################")
 
 #computer choice
 options = ("rock", "paper", "scissors")
-computer_choice = random.choice(options)
+
 
 while playing:
+    computer_choice = random.choice(options)
     while True:
         user_choice = input("Enter your choice (rock, paper and scissors): ").lower()
         if(options.count(user_choice) == 0):
@@ -26,18 +27,18 @@ while playing:
     # Game decision
     if(computer_choice==user_choice):
         print("Draw!!")
-    elif(computer_choice=="r" and user_choice=="s"):
+    elif(computer_choice=="rock" and user_choice=="scissors"):
         print("You Loss")
-    elif(computer_choice=="s" and user_choice=="p"):
+    elif(computer_choice=="scissors" and user_choice=="paper"):
         print("You Loss!!")
-    elif(computer_choice=="p" and user_choice=="r"):
+    elif(computer_choice=="paper" and user_choice=="rock"):
         print("You Loss!!")
     else:
         print("You Win!!")
         
         
     while True:
-        play = input("Do You want to play (y for yes or n for no)").lower()
+        play = input("Do You want to play (y for yes or n for no)?:").lower()
         if (play=="y"):
             playing=True
             break
